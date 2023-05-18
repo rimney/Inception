b3875d92a88df40a64c30d67c84aaba713ab7c91
rimney <rmneyz@gmail.com>
2023-05-18 03:59:21 +0100

    e

--- srcs/mariaDB/tools/setup_db_user.sh
+++ srcs/mariaDB/tools/setup_db_user.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 service mysql start
 # Creating a Database and User for WordPress
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
-mysql -u root -e "GRANT ALL ON $MYSQL_HOSTNAME.* TO '$MYSQL_USER'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root  mariadb < database.sql
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+# mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+# mysql -u root -e "GRANT ALL ON $MYSQL_HOSTNAME.* TO '$MYSQL_USER'@'%';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
+# mysql -u root  mariadb < database.sql
+#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
