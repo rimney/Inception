37e655c3f88d7dc24c5e32d59c64204740125d49
rimney <rmneyz@gmail.com>
2023-05-19 22:44:41 +0100

    all set up need to setup the makefile

--- srcs/wordpress/tools/run.sh
+++ srcs/wordpress/tools/run.sh
@@ -6,6 +6,6 @@ wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQ
 
 wp core install --url=${DOMAIN_NAME} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
-wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PW} --allow-root
+# wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PW} --allow-root
 
 exec $@
