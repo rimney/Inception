5182d3f0bba19169936738cf03a31653feffa9a4
rimney <rmneyz@gmail.com>
2023-05-19 07:54:36 +0100

    wordpress is good

--- srcs/mariaDB/tools/setup_db_user.sh
+++ srcs/mariaDB/tools/setup_db_user.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 service mysql start
-# Creating a Database and User for WordPress
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -e "GRANT ALL ON $MYSQL_HOSTNAME.* TO '$MYSQL_USER'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-# mysql -u root  mariadb < database.sql
-#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+
+mariadb -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mariadb -u root -e "FLUSH PRIVILEGES;"
+
+echo "Database and user created successfully!"
--- srcs/wordpress/tools/run.sh
+++ srcs/wordpress/tools/run.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
-service php7.3-fpm start
-service php7.3-fpm stop
-exec "$@"
+#!/bin/bash
+
+wp core download --allow-root
+
+wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOSTNAME} --allow-root
+
+wp core install --url=${DOMAIN_NAME} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+
+# wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PW} --allow-root
+
+exec $@
